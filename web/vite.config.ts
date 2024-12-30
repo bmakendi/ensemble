26ca94e6ba4d8b66fd174c311c554af965ebaf39
Bryan Makendi <bryan.makendi@gmail.com>
2024-12-30 01:44:54 +0100

    chore: work on implementing vitest

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,12 @@
+/// <reference types="vitest/config" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(() => ({
   plugins: [react()],
-})
+  test: {
+    globals: true,
+  },
+}))
