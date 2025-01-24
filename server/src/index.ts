d4fbdd9733201ac6c3b55b9724c40b61a94f6eae
Bryan Makendi <bryan.makendi@gmail.com>
2025-01-25 00:52:34 +0100

    chore: added tsx to run server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,3 +1,16 @@
-export function sum() {
-  return 0
-}
+import http from 'http'
+import { Server } from 'socket.io'
+
+const server = http.createServer()
+const io = new Server(server, { cors: { origin: '*' } })
+
+io.on('connection', (socket) => {
+  console.log('someone just logged in')
+  socket.on('logout', () => {
+    console.log('that someone bailed out')
+  })
+})
+
+server.listen(8000, () => {
+  console.log('server running on port 8000')
+})
