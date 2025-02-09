36a1e3b3772c5fa98afcc912b05ad6cf61c6ae10
Bryan Makendi <bryan.makendi@gmail.com>
2025-02-09 23:04:49 +0100

    chore: add eslint and init server

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { Server } from 'socket.io'
+
+const io = new Server(3000, {
+  cors: { origin: '*' },
+  transports: ['websocket'],
+})
+
+console.log('Server running on port 3000!')
+
+io.on('connection', (socket) => {
+  console.log('connected')
+})
